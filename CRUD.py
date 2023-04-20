687992cf040ac0c8eb6170c192f47b4c90a04741
Yanirw <117165801+Yanirw@users.noreply.github.com>
2023-04-21 00:46:32 +0300

    improved version

--- CRUD.py
+++ CRUD.py
@@ -1,28 +1,60 @@
-# This script performs CRUD operations on a RESTful API
+# Improved script for performing CRUD operations on a RESTful API
 
 import requests
 
-api_url = "https://api.example.com/resources"
+# Constants
+API_URL = "https://api.example.com/resources"
+
+def create_resource(payload):
+    response = requests.post(API_URL, json=payload)
+    if response.status_code == 201:
+        print("Resource created successfully.")
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def read_resources():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        print("Resources retrieved successfully.")
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def update_resource(resource_id, payload):
+    update_url = f"{API_URL}/{resource_id}"
+    response = requests.put(update_url, json=payload)
+    if response.status_code == 200:
+        print("Resource updated successfully.")
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def delete_resource(resource_id):
+    delete_url = f"{API_URL}/{resource_id}"
+    response = requests.delete(delete_url)
+    if response.status_code == 204:
+        print("Resource deleted successfully.")
+    else:
+        print(f"Error: {response.status_code}")
+
+# Usage examples:
 
 # CREATE
-payload = {"key": "value"}
-response = requests.post(api_url, json=payload)
-print(response.json())
+new_payload = {"key": "value"}
+new_resource = create_resource(new_payload)
 
 # READ
-response = requests.get(api_url)
-print(response.json())
+resources = read_resources()
 
 # UPDATE
-resource_id = 1
-update_url = f"{api_url}/{resource_id}"
+resource_id_to_update = 1
 update_payload = {"key": "new_value"}
-response = requests.put(update_url, json=update_payload)
-print(response.json())
+updated_resource = update_resource(resource_id_to_update, update_payload)
 
 # DELETE
-# DELETE
-resource_id = 1
-delete_url = f"{api_url}/{resource_id}"
-response = requests.delete(delete_url)
-print(response.status_code)
+resource_id_to_delete = 1
+delete_resource(resource_id_to_delete)
